830dcdfd3dcb9c6edddc586d46b67ab37b76422a
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-15 14:39:04 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    title: "Ini judul artikel",
+    body: "Body dari artikel pertama. Apapun bisa ditulis disini",
+  },
+  {
+    id: 2,
+    title: "Artikel kedua",
+    body: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 3,
+    title: "Third article",
+    body: "This is another article, idk what to type",
+  },
+  {
+    id: 4,
+    title: "Fourth Empat Yon",
+    body: "Just try to write whatever came on my mind",
+  },
+];
+
+module.exports = data;
