dc40ef6b5aa29aa8d253a1a9dc5ca0588be66991
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-15 14:39:04 +0800

    Creating post method

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const articles = require("./data");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/articles", (req, res) => {
   res.status(200).json(articles);
 });
@@ -11,6 +14,25 @@ app.get("/api/articles", (req, res) => {
 app.get("/api/articles/:id", (req, res) => {
   const article = articles.find((e) => e.id === Number(req.params.id));
   res.status(200).json(article);
+});
+
+app.post("/api/articles", (req, res) => {
+  // Destructuring
+  const { title, body } = req.body;
+
+  // Dapatkan ID dari item terakhir
+  const lastId = articles[articles.length - 1].id;
+  const newId = lastId + 1;
+
+  const article = {
+    id: newId,
+    title: title,
+    body: body,
+  };
+
+  articles.push(article);
+
+  res.status(201).json(article);
 });
 
 app.listen(port, () => {
